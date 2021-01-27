26a6305585f5f4cbd7349c5392bd1fd52da11f71
Дуйсеналиев Алиби Ерланулы <@corp.itstep>
2021-01-27 19:14:09 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/animate.min.css">
+</head>
+<body>
+   <div class="container">
+       <div class="row">
+           <div class="col-md-6 col-xs-12 wow bounceInLeft">
+               <div class="block"></div>
+           </div>
+           <div class="col-md-6 col-xs-12 wow bounceInRight">
+                <div class="block"></div>
+            </div>
+       </div>
+   </div>
+
+   <a href="a.jpg" download="a.png">aaaaa</a>
+
+   <script src="js/wow.min.js"></script>
+              <script>
+              new WOW().init();
+    </script>
+</body>
+</html>
